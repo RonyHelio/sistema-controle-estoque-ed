ad8c58d77be4eed577e3aa5bbbcfaa7f2d1c2a9a
Gusthavo <gusthavogp123@gmail.com>
2025-08-06 13:12:26 -0300

    Interface Grafica e algumas funcoes

---
+++ LojaAgricola.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lojaagricola;
+
+/**
+ *
+ * @author Usuario
+ */
+public class LojaAgricola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Produto.java
@@ -0,0 +1,45 @@
+package lojaagricola;
+
+public class Produto {
+    private String nome;
+    private double preço;
+    private int estoque;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, double preço, int estoque) {
+        this.nome = nome;
+        this.preço = preço;
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return nome + ", " + estoque + " disponiveis. preço: " + preço;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+    public void setPreço(double preço) {
+        this.preço = preço;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
